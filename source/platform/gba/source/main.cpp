4d5726554c6925a19b6aeff83b1828c321070caf
Mark Richardson <oldkentuckymark@gmail.com>
2026-07-22 12:17:58 -0400

    switch to mode 5 for gba

--- source/platform/gba/source/main.cpp
+++ source/platform/gba/source/main.cpp
@@ -84,14 +84,14 @@ public:
 class Context final : public ffr::Context<FFT>
 {
 public:
+
     Context()
     {
+        // Enable Vblank Interrupt to allow VblankIntrWait
+        irqEnable(IRQ_VBLANK);
+        SetMode( MODE_5 | BG2_ON );
     }
 
-    ~Context()
-    {
-
-    }
 
     inline void clear() override
     {
@@ -130,31 +130,86 @@ private:
     constexpr static uint16_t width = 160;
     constexpr static uint16_t height = 128;
 
-    //uint16_t * FRONT =
+    volatile uint16_t * FB = (uint16_t*)0x6000000;
+    volatile uint16_t * BB = (uint16_t*)0x600A000;
 
-    volatile uint16_t * vram = reinterpret_cast<uint16_t*>(0x06000000);;
+
+    volatile uint16_t * vram = BB;
 
     inline void flipPage()
     {
-        //return;
-        // Wait for VBlank to avoid tearing
-        //while (*(volatile u16*)0x04000006 >= 160); // Wait until VDraw ends
-        //while (*(volatile u16*)0x04000006 < 160);  // Wait until VBlank ends
 
-        // Toggle display page
-        if (REG_DISPCNT & 0x0010)
+        //while(REG_VCOUNT >= 160); // Wait for vertical blank
+
+        if(REG_DISPCNT & 0x10)
         {
-            REG_DISPCNT &= ~(0x0010); // Show front buffer (0x06000000)
-            vram = reinterpret_cast<uint16_t*>(0x0600A000);  // Now draw to back buffer
+            REG_DISPCNT &= ~0x10; // Show Frame 0
+            vram = BB;
         }
         else
         {
-            REG_DISPCNT |= 0x0010;  // Show back buffer (0x0600A000)
-            vram = reinterpret_cast<uint16_t*>(0x06000000); // Now draw to front buffer
+            REG_DISPCNT |= 0x10;  // Show Frame 1
+            vram = FB;
         }
     }
 
 };
+
+
+
+/*
+class Context final : public ffr::Context<FFT>
+{
+public:
+
+    Context()
+    {
+        // Enable Vblank Interrupt to allow VblankIntrWait
+        irqEnable(IRQ_VBLANK);
+        SetMode( MODE_3 | BG2_ON );
+    }
+
+    inline void clear() override
+    {
+        for(volatile uint16_t* p = vram;p < vram+(width*height);++p)
+        {
+            *p = 0;
+        }
+    }
+
+    inline void present() override
+    {
+
+    }
+
+    inline void plot(int16_t x, int16_t y, uint16_t c) override
+    {
+        vram[(y*width)+x] = c;
+    }
+
+    inline void lineHorizontal(int16_t x0, int16_t y0, int16_t x1, uint16_t color) override
+    {
+        if(x0 > x1)
+        {
+            auto tmp = x0;
+            x0 = x1;
+            x1 = tmp;
+        }
+
+        for(uint16_t* p = (uint16_t*)&vram[(y0*width)+x0]; p <= &vram[(y0*width)+x1]; ++p)
+        {
+            *p = color;
+        }
+    }
+
+private:
+    constexpr static uint16_t width = 240;
+    constexpr static uint16_t height = 160;
+
+    volatile uint16_t * vram = (uint16_t*)0x6000000;
+
+};
+*/
 
 
 uint32_t getKeyState(uint16_t key_code)
@@ -170,9 +225,7 @@ int main(void)
 
     //irqSet( IRQ_VBLANK, VblankInterrupt);
 
-    // Enable Vblank Interrupt to allow VblankIntrWait
-    irqEnable(IRQ_VBLANK);
-    SetMode( MODE_5 | BG2_ON );		// screen mode & background to display
+
 
 
     Context ctx;
@@ -187,8 +240,8 @@ int main(void)
         bool inputs[10];
         //inputs[static_cast<uint16_t>(Game::Input::A)] = getKeyState(KEY_A);
 
-        VBlankIntrWait();
-                    p1 = p1 + ffm::vec3{0.07_fx,0.00_fx,0.07_fx};
+
+        p1 = p1 + ffm::vec3{0.07_fx,0.00_fx,0.07_fx};
 
         ctx.clear();
         ctx.getVertexFunction().camPos = cp;
@@ -204,7 +257,7 @@ int main(void)
         ctx.setVertexPointer(3,sizeof(Vertex),SHIPMESH.data());
         ctx.drawArray(ffr::DrawType::Triangles,0,SHIPMESH.size());
 
-
+        ctx.present();
 
 
     }
